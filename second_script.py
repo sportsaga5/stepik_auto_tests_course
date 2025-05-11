fdbacaa2fb83214e2443db67b22a514c22488531
Valpeg <valpegushin@yandex.ru>
2025-05-11 18:21:21 +0500

    add python file from previous module

---
+++ second_script.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+try:
+    link = "https://suninjuly.github.io/registration2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    get_all_fields = browser.find_elements(By.CSS_SELECTOR, "input[required]")
+    for field in get_all_fields:
+        field.send_keys("test")
+        time.sleep(1)
+    # time.sleep(3)
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+    if "Congratulations! You have successfully registered!" == welcome_text:
+        print("Тест пройден")
+    else:
+        print("Тест не пройден")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
